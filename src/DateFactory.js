6d4ec54e169fcc0f4e6c758c302a1bd050361914
Edwin Hoogerbeets <ehoogerbeets@box.com>
2018-10-26 16:17:37 -0700

    Add support for dates and date formats

---
+++ src/DateFactory.js
@@ -0,0 +1,42 @@
+/**
+ * DateFactory.js - ES6 wrappers around an ilib class
+ *
+ * @license
+ * Copyright © 2018, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { promisifyFunction } from './promisify';
+
+const ilibDateFactory = require('ilib/lib/DateFactory.js');
+
+export function DateFactoryAsync(options = {}) {
+    var opts = { ...options };
+    opts.sync = false;
+    return promisifyFunction(ilibDateFactory, opts);
+};
+
+function DateFactory(options = {}) {
+    const { sync } = options;
+    if (typeof(sync) === 'boolean' && !sync) {
+        return DateFactoryAsync(options);
+    }
+
+    return ilibDateFactory(options);
+};
+
+DateFactory._dateToIlib = ilibDateFactory._dateToIlib;
+
+export default DateFactory;
