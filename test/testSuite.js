37ab3547f023c1872cbaa89283ecf7da082f5537
Edwin Hoogerbeets <ehoogerbeets@box.com>
2019-06-18 14:07:54 -0700

    require the new babel instead of the old one

--- test/testSuite.js
+++ test/testSuite.js
@@ -1,7 +1,7 @@
 /*
  * testSuite.js - test suite for this directory
  * 
- * Copyright © 2018, JEDLSoft
+ * Copyright © 2018-2019, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ var suites = require("./testSuiteFiles.js").files;
 
 // this processes all subsequent requires using babel
 process.env.BABEL_ENV = "test";
-require("babel-register");
+require("@babel/register");
 
 suites.forEach(function (path) {
     var test = require("./" + path);
