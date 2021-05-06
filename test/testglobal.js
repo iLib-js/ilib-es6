2b6fdfb4dc7eb3d6ba69707ca92e56922f45a6d6
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2021-05-06 14:48:14 -0700

    Update to ilib v14.9.0

--- test/testglobal.js
+++ test/testglobal.js
@@ -66,7 +66,7 @@ module.exports.testglobal = {
     
     testGetVersion: function(test) {
         test.expect(1);
-        test.equal(ilib.getVersion().substring(0,4), "14.8");
+        test.equal(ilib.getVersion().substring(0,4), "14.9");
         test.done();
     },
     
