d9506a7aaad0d479d5292bc7d688d8182499964a
Edwin Hoogerbeets <ehoogerbeets@gmail.com>
2024-01-31 21:06:07 -0800

    Update copyright notices in comments and README

--- README.md
+++ README.md
@@ -155,7 +155,7 @@ CalendarFactoryAsync({locale: 'ja-JP'}).then(function(cal) {
 
 ## License
 
-Copyright © 2019-2023, JEDLSoft
+Copyright © 2019-2024, JEDLSoft
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- src/isGraph.js
+++ src/isGraph.js
@@ -2,7 +2,7 @@
  * isGraph.js - ES6 wrappers around an ilib class
  *
  * @license
- * Copyright © 2018, 2022 JEDLSoft
+ * Copyright © 2018, 2022, 2024 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
